58ed364b4e1b29b0bec25b21a01a230a75740470
Jakob Heuser <jakob@felocity.com>
2020-02-05 10:38:42 -0800

    Removes redundant test
    Originally, I wanted to see if we could modify handleBeforeInput such that
    it would support both v0.10 and v0.11. Unfortunately, counting the
    number of args was unreliable when multiple plugins were involved. This
    test was made redundant by updating tests to require an event timestamp
    when calling handleBeforeInput

--- src/__test__/plugin-new.test.js
+++ src/__test__/plugin-new.test.js
@@ -1,9 +1,9 @@
-import Draft, { EditorState, SelectionState, convertToRaw } from "draft-js";
-import createMarkdownPlugin from "../";
-import { applyMDtoInlineStyleChange } from "./utils";
+import Draft, { EditorState, SelectionState, convertToRaw } from 'draft-js';
+import createMarkdownPlugin from '../';
+import { applyMDtoInlineStyleChange } from './utils';
 
-describe("markdown", () => {
-  it("should work with Draft v11 syntax", () => {
+describe('markdown', () => {
+  it('should convert asteriks to bold text', () => {
     const { handleBeforeInput } = createMarkdownPlugin();
     const setEditorState = jest.fn();
     const before = EditorState.moveSelectionToEnd(
@@ -12,9 +12,9 @@ describe("markdown", () => {
           entityMap: {},
           blocks: [
             {
-              key: "item1",
-              text: "Some *text",
-              type: "unstyled",
+              key: 'item1',
+              text: 'Some *text',
+              type: 'unstyled',
               depth: 0,
               inlineStyleRanges: [],
               entityRanges: [],
@@ -25,53 +25,21 @@ describe("markdown", () => {
       )
     );
     expect(
-      handleBeforeInput("*", before, new Date().getTime(), {
-        setEditorState,
-      })
-    ).toEqual("handled");
+      handleBeforeInput('*', before, new Date().getTime(), { setEditorState })
+    ).toEqual('handled');
     const raw = convertToRaw(
       setEditorState.mock.calls[0][0].getCurrentContent()
     );
     expect(raw).toMatchSnapshot();
   });
 
-  it("should convert asteriks to bold text", () => {
-    const { handleBeforeInput } = createMarkdownPlugin();
-    const setEditorState = jest.fn();
-    const before = EditorState.moveSelectionToEnd(
-      EditorState.createWithContent(
-        Draft.convertFromRaw({
-          entityMap: {},
-          blocks: [
-            {
-              key: "item1",
-              text: "Some *text",
-              type: "unstyled",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-          ],
-        })
-      )
-    );
-    expect(
-      handleBeforeInput("*", before, new Date().getTime(), { setEditorState })
-    ).toEqual("handled");
-    const raw = convertToRaw(
-      setEditorState.mock.calls[0][0].getCurrentContent()
-    );
-    expect(raw).toMatchSnapshot();
-  });
-
-  it("should not do anything to existing inline styles when within them", () => {
+  it('should not do anything to existing inline styles when within them', () => {
     const { handleBeforeInput } = createMarkdownPlugin();
     const setEditorState = jest.fn();
     const boldInlineStyleRange = {
       length: 4,
       offset: 5,
-      style: "BOLD",
+      style: 'BOLD',
     };
     const before = EditorState.forceSelection(
       EditorState.createWithContent(
@@ -79,9 +47,9 @@ describe("markdown", () => {
           entityMap: {},
           blocks: [
             {
-              key: "item1",
-              text: "Some text",
-              type: "unstyled",
+              key: 'item1',
+              text: 'Some text',
+              type: 'unstyled',
               depth: 0,
               inlineStyleRanges: [boldInlineStyleRange],
               entityRanges: [],
@@ -91,27 +59,27 @@ describe("markdown", () => {
         })
       ),
       new SelectionState({
-        anchorKey: "item1",
+        anchorKey: 'item1',
         anchorOffset: 6,
-        focusKey: "item1",
+        focusKey: 'item1',
         focusOffset: 6,
         isBackward: false,
         hasFocus: true,
       })
     );
     expect(
-      handleBeforeInput("a", before, new Date().getTime(), {
+      handleBeforeInput('a', before, new Date().getTime(), {
         setEditorState,
       })
-    ).toEqual("not-handled");
+    ).toEqual('not-handled');
   });
 
-  it("should not unstick inline styles if they were not added by md-to-inline-style change", () => {
+  it('should not unstick inline styles if they were not added by md-to-inline-style change', () => {
     const { handleBeforeInput } = createMarkdownPlugin();
     const boldInlineStyleRange = {
       length: 4,
       offset: 5,
-      style: "BOLD",
+      style: 'BOLD',
     };
     const editorState = EditorState.moveSelectionToEnd(
       EditorState.createWithContent(
@@ -119,9 +87,9 @@ describe("markdown", () => {
           entityMap: {},
           blocks: [
             {
-              key: "item1",
-              text: "Some text",
-              type: "unstyled",
+              key: 'item1',
+              text: 'Some text',
+              type: 'unstyled',
               depth: 0,
               inlineStyleRanges: [boldInlineStyleRange],
               entityRanges: [],
@@ -132,17 +100,17 @@ describe("markdown", () => {
       )
     );
     expect(
-      handleBeforeInput("a", editorState, new Date().getTime(), {})
-    ).toEqual("not-handled");
+      handleBeforeInput('a', editorState, new Date().getTime(), {})
+    ).toEqual('not-handled');
   });
 
-  it("should not have sticky inline styles", () => {
+  it('should not have sticky inline styles', () => {
     const { handleBeforeInput } = createMarkdownPlugin();
     const setEditorState = jest.fn();
     const boldInlineStyleRange = {
       length: 4,
       offset: 5,
-      style: "BOLD",
+      style: 'BOLD',
     };
     const editorState = applyMDtoInlineStyleChange(
       EditorState.moveSelectionToEnd(
@@ -151,9 +119,9 @@ describe("markdown", () => {
             entityMap: {},
             blocks: [
               {
-                key: "item1",
-                text: "Some text",
-                type: "unstyled",
+                key: 'item1',
+                text: 'Some text',
+                type: 'unstyled',
                 depth: 0,
                 inlineStyleRanges: [boldInlineStyleRange],
                 entityRanges: [],
@@ -166,10 +134,10 @@ describe("markdown", () => {
     );
 
     expect(
-      handleBeforeInput("a", editorState, new Date().getTime(), {
+      handleBeforeInput('a', editorState, new Date().getTime(), {
         setEditorState,
       })
-    ).toEqual("handled");
+    ).toEqual('handled');
     const raw = convertToRaw(
       setEditorState.mock.calls[0][0].getCurrentContent()
     );
@@ -177,13 +145,13 @@ describe("markdown", () => {
     expect(raw).toMatchSnapshot();
   });
 
-  it("should not have sticky inline styles after the line ending with styles", () => {
+  it('should not have sticky inline styles after the line ending with styles', () => {
     const { handleBeforeInput } = createMarkdownPlugin();
     const setEditorState = jest.fn();
     const boldInlineStyleRange = {
       length: 4,
       offset: 5,
-      style: "BOLD",
+      style: 'BOLD',
     };
     const editorState = applyMDtoInlineStyleChange(
       EditorState.moveSelectionToEnd(
@@ -192,18 +160,18 @@ describe("markdown", () => {
             entityMap: {},
             blocks: [
               {
-                key: "item1",
-                text: "Some text",
-                type: "unstyled",
+                key: 'item1',
+                text: 'Some text',
+                type: 'unstyled',
                 depth: 0,
                 inlineStyleRanges: [boldInlineStyleRange],
                 entityRanges: [],
                 data: {},
               },
               {
-                key: "item2",
-                text: "",
-                type: "unstyled",
+                key: 'item2',
+                text: '',
+                type: 'unstyled',
                 depth: 0,
                 inlineStyleRanges: [],
                 entityRanges: [],
@@ -216,10 +184,10 @@ describe("markdown", () => {
     );
 
     expect(
-      handleBeforeInput("a", editorState, new Date().getTime(), {
+      handleBeforeInput('a', editorState, new Date().getTime(), {
         setEditorState,
       })
-    ).toEqual("handled");
+    ).toEqual('handled');
     const raw = convertToRaw(
       setEditorState.mock.calls[0][0].getCurrentContent()
     );
